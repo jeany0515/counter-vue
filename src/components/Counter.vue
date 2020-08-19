612e68dcff9e99a0cd1835cf59968d7c33d78926
devxu <devxu@thoughtworks.com>
2020-08-19 15:45:57 +0800

    [counter][jeany]: add counter and remove unused code

---
+++ src/components/Counter.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="counter">
+        <button @click="increase()">+</button>
+        <span>{{count}}</span>
+        <button @click="decrease()">-</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data() {
+            return {
+                count: 0
+            }
+        },
+        methods: {
+            increase() {
+                this.count = this.count + 1;
+            },
+
+            decrease() {
+                this.count = this.count - 1;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
